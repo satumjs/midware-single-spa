733d7c2202027b89e61114ce0c98a3a5fd1e84eb
valleykid <valleykiddy@gmail.com>
2021-08-14 13:31:47 +0800

    fix: remove lib

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { MidwareSystem, RealMicroApp, NextFn } from '@satumjs/types';
-
-declare function singleSpaMidware(system: MidwareSystem, microApps: RealMicroApp[], next: NextFn): void;
-
-export { singleSpaMidware as default };
