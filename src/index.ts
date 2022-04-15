ea65fc48b66dd63f7889513f51534c8c7696220b
valleykid <valleykiddy@gmail.com>
2022-04-15 08:47:08 +0800

    release 0.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MidwareSystem, RealMicroApp, NextFn } from '@satumjs/types';
+import { MidwareSystem, IMicroApp, NextFn, MidwareName } from '@satumjs/types';
 import {
   registerApplication,
   start as sspaStart,
@@ -12,20 +12,22 @@ import {
 } from 'single-spa';
 
 export { addErrorHandler, removeErrorHandler };
-export default function singleSpaMidware(system: MidwareSystem, microApps: RealMicroApp[], next: NextFn) {
+export default function singleSpaMidware(system: MidwareSystem, microApps: IMicroApp[], next: NextFn) {
   const defaultTimeout = 4000; // single-spa default timeout
-  const { timeout, bootstrapMaxTime, mountMaxTime, unmountMaxTime, unloadMaxTime, errorHandler } = system.options;
+  const { timeout, bootstrapMaxTime, mountMaxTime, unmountMaxTime, unloadMaxTime, errorHandler, urlRerouteOnly } = system.options;
 
   setBootstrapMaxTime(bootstrapMaxTime || timeout || defaultTimeout);
   setMountMaxTime(mountMaxTime || timeout || defaultTimeout);
   setUnmountMaxTime(unmountMaxTime || timeout || defaultTimeout);
   setUnloadMaxTime(unloadMaxTime || timeout || defaultTimeout);
 
-  microApps.forEach(({ name, app, activeWhen, customProps }) => {
-    registerApplication({ name, app, activeWhen, customProps } as RegisterApplicationConfig);
+  system.set(MidwareName.start, () => {
+    microApps.forEach(({ name, app, activeWhen, customProps }) => {
+      const config = { name, app, activeWhen, customProps } as RegisterApplicationConfig;
+      registerApplication(config);
+    });
+    typeof urlRerouteOnly === 'undefined' ? sspaStart() : sspaStart({ urlRerouteOnly });
   });
-
-  system.set('start', sspaStart);
 
   if (errorHandler) {
     addErrorHandler(errorHandler);
