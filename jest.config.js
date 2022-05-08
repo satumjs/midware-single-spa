b787f12cbe043894d9aaaf7e4dd37d423c08b527
valleykid <valleykiddy@gmail.com>
2022-05-09 06:55:48 +0800

    chore: coveralls get coverage data

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: { '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest' },
   testTimeout: 30000,
-  coverageReporters: ['text', 'html', 'lcov'],
+  coverageReporters: ['json', 'text', 'html', 'lcov', 'clover'],
   testEnvironment: 'jsdom'
 };
